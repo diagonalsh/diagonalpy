cd181f3a4cf778c21f9f51c47962ee38094313e8
Leon Luithlen <leontimnaluithlen@gmail.com>
2025-01-14 16:52:15 +0100

    Remove surplus imports in tests

--- tests/integration/test_conversion.py
+++ tests/integration/test_conversion.py
@@ -1,9 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from diagonalpy.convert import convert
-from diagonalpy.sklearn.linear_model import (
-    convert_sklearn_linear_to_pytorch,
-)
 
 
 def test_convert_linear_regression():
@@ -12,7 +9,6 @@ def test_convert_linear_regression():
     y = np.sum(X, axis=1) + np.random.randn(50)
     lr.fit(X, y)
     convert(lr)
-    convert_sklearn_linear_to_pytorch(lr)
 
 
 def test_convert_logistic_regression_multiclass():
