f76820df4a520cbe503a7dd8b651be00e1a76cae
Leon Luithlen <leontimnaluithlen@gmail.com>
2025-01-14 16:45:11 +0100

    Add logistic regression

--- src/diagonalpy/export.py
+++ src/diagonalpy/export.py
@@ -1,11 +1,16 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from typing import Any
-from diagonalpy.sklearn.linear_model import export_linear_regression
+from diagonalpy.sklearn.linear_model import (
+    export_linear_regression,
+    export_logistic_regression,
+)
 
 
 def export(model: Any) -> None:
     if isinstance(model, LinearRegression):
         pytorch_model = export_linear_regression(model)
+    elif isinstance(model, LogisticRegression):
+        pytorch_model = export_logistic_regression(model)
     else:
         raise NotImplementedError(f"Export not currently implemented for {type(model)}")
 
--- tests/integration/test_export.py
+++ tests/integration/test_export.py
@@ -1,15 +1,39 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from diagonalpy.export import export
 from diagonalpy.sklearn.linear_model import (
     convert_sklearn_linear_to_pytorch,
 )
 
 
-def test_export():
+def test_export_linear_regression():
     lr = LinearRegression()
     X = np.random.randn(50, 10)
     y = np.sum(X, axis=1) + np.random.randn(50)
     lr.fit(X, y)
     export(lr)
     convert_sklearn_linear_to_pytorch(lr)
+
+
+def test_export_logistic_regression_multiclass():
+    clf = LogisticRegression()
+    X = np.random.randn(50, 10)
+    y = ((np.sum(X, axis=1) + np.random.randn(50)) * 0.5).astype(int)
+    y = y - np.min(y)
+    y[y < 2] = 2
+    y[y > 4] = 4
+    y = y - np.min(y)
+    clf.fit(X, y)
+    export(clf)
+
+
+def test_export_logistic_regression_binary():
+    clf = LogisticRegression()
+    X = np.random.randn(50, 10)
+    y = ((np.sum(X, axis=1) + np.random.randn(50)) * 0.5).astype(int)
+    y = y - np.min(y)
+    y[y < 2] = 2
+    y[y > 3] = 3
+    y = y - np.min(y)
+    clf.fit(X, y)
+    export(clf)
